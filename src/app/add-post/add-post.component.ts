94b76292eda97ab3f8ae41df180ec20c97768c55
jobin <jobinjose.in@gmail.com>
2022-11-29 08:40:05 +0530

    readvalues

--- src/app/add-post/add-post.component.ts
+++ src/app/add-post/add-post.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-post.component.css']
 })
 export class AddPostComponent {
+  userid=""
+  id=""
+  title=""
+  body=""
+  readValues = ()=>{
+    let data:any = {
+      "userid":this.userid,
+      "id":this.id,
+      "title":this.title,
+      "body":this.body
+    }
+    console.log(data)
+  }
 
 }
